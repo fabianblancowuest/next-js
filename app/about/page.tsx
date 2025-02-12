d3e99f60352dddc57428cf8397705ce286add5fc
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2025-02-11 23:53:28 -0300

    Rutas adicionales

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+	return (
+		<>
+			<h2 className="text-7xl">About Page</h2>
+		</>
+	);
+};
+
+export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ export default function Home() {
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<span className="text-5xl">Hola Mundo!!!</span>
+				<h2 className="text-5xl">Hola Mundo!!! La fecha de hoy es:</h2>
+				<h3 className="text-4xl text-center block mx-auto">
+					{new Date().toLocaleDateString("es-Es")}
+				</h3>
 			</main>
 			{/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer> */}
 		</div>
